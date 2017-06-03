57efe16d77f6c627700faa1611cd848f67e0a016
Bethany Watson <bwatson2@binghamton.edu>
2017-06-02 23:13:10 -0400

    Refactor javascript.

--- app/assets/javascripts/scribble_store/scribbles.js
+++ app/assets/javascripts/scribble_store/scribbles.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+initScribbleCropper = function(aspectRatio) {
+  var updateCropper = function(coords) {
+    $('#scribble_crop_x').val(coords.x);
+    $('#scribble_crop_y').val(coords.y);
+    $('#scribble_crop_w').val(coords.w);
+    $('#scribble_crop_h').val(coords.h);
+  };
+
+  var width = parseInt($('#cropbox').width());
+  var height = parseInt($('#cropbox').height());
+
+  $('#cropbox').Jcrop({
+    aspectRatio: aspectRatio,
+    setSelect: [0, 0, width, height],
+    onSelect: updateCropper,
+    onChange: updateCropper
+  });
+};
