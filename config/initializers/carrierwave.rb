1d2b2841e1b2d4333f907d9b2017018406d7e4f6
Bethany Watson <bwatson2@binghamton.edu>
2017-10-03 08:57:46 -0400

    Move AWS config.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,14 @@ CarrierWave.configure do |config|
   # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
-      config.storage = :file
+       config.fog_credentials = {
+        :provider               => 'AWS',                             # required
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']     # required
+      }
+      config.storage = :fog
+      config.fog_directory  = 'dot-me-scribbles'               # required
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
   end
 
@@ -34,14 +41,4 @@ CarrierWave.configure do |config|
       end
     end
   end
-
-
-  
-  config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => '<your key goes here>',            # required
-    :aws_secret_access_key  => '<your secret key goes here>'     # required
-  }
-  config.fog_directory  = 'dot-me-scribbles'               # required
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
