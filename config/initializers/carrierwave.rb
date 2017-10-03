c10407450e25d8be36d251cf7633e161720d112b
Bethany Watson <bwatson2@binghamton.edu>
2017-10-03 09:08:33 -0400

    Set fog provider instead.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,14 +10,14 @@ CarrierWave.configure do |config|
   # Use AWS storage if in production
   if Rails.env.production? || Rails.env.development?
     CarrierWave.configure do |config|
-       config.fog_credentials = {
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
         :provider               => 'AWS',                             # required
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
         :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']     # required
       }
       config.fog_directory  = 'dot-me-scribbles'               # required
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-      config.storage = :fog
     end
   end
 
